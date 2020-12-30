c52c2793ad8d2a2782922f6b1fab8b6b04d3aa2c
vlykh <vlykh@softserveinc.com>
2020-12-30 08:47:35 +0200

    HW13 task 3 fix

--- HW13/_tests_/calcCircle.test.js
+++ HW13/_tests_/calcCircle.test.js
@@ -9,6 +9,14 @@ describe("Circle functions tests", () => {
   test("check function getCircleArea with 9", () => {
     expect(circle.getCircleArea(9)).toBeCloseTo(254.47, 2);
   });
+
+  test('Test the function getCircleLength with no arguments', () => {
+    expect(circle.getCircleLength()).toBeNaN();
+});
+test('Test the function getCircleArea with no arguments', () => {
+    expect(circle.getCircleArea()).toBeNaN();
+});
+
   test.each([[22, 138.2]])(
     "check circle length with radius %f is %f",
     (r, expected) => {
